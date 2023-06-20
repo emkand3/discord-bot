e613e45e15887409962aa070d8d172d970fcbd9f
Emma <emsca2002@gmail.com>
2023-06-20 11:43:00 -0400

    added registration system for anagram game

--- bot.py
+++ bot.py
@@ -3,11 +3,13 @@ from discord.ext import commands
 from random_word import Wordnik
 import random
 import json
+import asyncio
 
 anagram_game_started = False
 anagram_game_finished = False
 anagram_jumbled = ""
 anagram_normal = ""
+anagram_players = []
 
 # get token from hidden file
 file = open('config.json')
@@ -33,27 +35,60 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command(name="anagram")
 async def anagram_start(ctx):
   
+  #define global variables
   global anagram_game_started, anagram_normal, anagram_jumbled, anagram_game_finished
 
+  #makes sure a game is not in progress
   if anagram_game_started:
-    print("no no")
     await ctx.send("Game in progress!")
     return
-  print("command received")
+  
+  #gets the anagram word and normal word
   valid_word = False
   while not valid_word:
     word = r.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun,verb,adjective", minDictionaryCount=15, minLength=4)
-    print(word)
     if word:
       valid_word = True
   anagram = word_jumble(word)
   anagram_normal = word.strip()
   anagram_jumbled = anagram
-  print(anagram)
-  await ctx.send("Starting your anagram game...")
-  #TODO: add functionality for specific users joining a game
+
+  #start a timer for joining the game
+  timerVal = 10
+  start_message = await ctx.send(f"Anagram game starting in {timerVal}s.\nReact to join!")
+  await start_message.add_reaction("🎟")
+
+  while True:
+    timerVal -= 1
+    await start_message.edit(content=f"Anagram game starting in {timerVal}s.\nReact to join!")
+    if timerVal == 0:
+      players_message = await ctx.fetch_message(start_message.id)
+      for reaction in players_message.reactions:
+          async for user in reaction.users():
+            if user != bot.user:
+              anagram_players.append(user)
+      print(anagram_players)
+      break
+    await asyncio.sleep(1)
+
+  #start the anagram functionality
   await ctx.send("Your word is: " + anagram)
   anagram_game_started = True
+
+
+#test timer
+@bot.command(name="timer")
+async def timer(ctx, time):
+  timerVal = int(time)
+  message = await ctx.send(f"Timer: {timerVal}")
+  while True:
+    timerVal -= 1
+    if timerVal == 0:
+      await message.edit(content=f"Times up!")
+      break
+    await message.edit(content=f"Timer: {timerVal}")
+    await asyncio.sleep(1)
+
 
 #parses sent messages
 @bot.event
@@ -61,14 +96,16 @@ async def on_message(message):
   if message.author == bot.user:
     return
   await bot.process_commands(message)
-  global anagram_game_started, anagram_normal, anagram_jumbled, anagram_game_finished
-  if anagram_game_started:
+  global anagram_game_started, anagram_normal, anagram_jumbled, anagram_game_finished, anagram_players
+  if anagram_game_started and message.author in anagram_players:
     print(message.content + " vs " + anagram_normal)
-    if message.content == anagram_normal:
-      print("yassss done")
+    if message.content.lower() == anagram_normal:
+      await message.reply(f"You did it! The word was {anagram_normal}")
       anagram_game_started = False
       anagram_game_finished = True
-      await message.reply("yass you did it")
+      anagram_normal = ""
+      anagram_jumbled = ""
+      anagram_players = []
 
 
 bot.run(token['token'])
